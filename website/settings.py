4148f17da209deb23a4c27a8aa954e6f36a0e6e9
Alireza Razavi <alirezaalavirazavi@yahoo.com>
2020-09-12 13:48:02 -0400

    wtfff

--- website/settings.py
+++ website/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'damir',
-        'USER': 'jeremih',
-        'PASSWORD': 'hellomother',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'damir',
+#         'USER': 'jeremih',
+#         'PASSWORD': 'hellomother',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
